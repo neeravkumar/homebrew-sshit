5afd2887e142b9489ccca4ed99577cf2d6a0044d
Neerav Kumar <me@nero.im>
2015-04-20 15:07:36 +0800

    add version

--- sshit.rb
+++ sshit.rb
@@ -1,6 +1,7 @@
 require "formula"
 
 class Sshit < Formula
+  version "0.1"
   homepage "https://github.com/thcipriani/sshit"
   url "https://github.com/thcipriani/sshit/archive/5c87d9e082d5b9e7e9f7cde531de1e016b6b73bc.tar.gz"
   sha1 "57ddd6a61d2235adb5d8def3290f8d595753fc45"
